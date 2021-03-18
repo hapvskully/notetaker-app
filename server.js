1028fe841cb81b6ce638d965a6f5f77b7d889236
hapvskully <77029963+hapvskully@users.noreply.github.com>
2021-03-18 18:48:59 -0400

    fixed delete button

--- server.js
+++ server.js
@@ -39,6 +39,26 @@ app.post('/api/notes', (req, res) => {
     })
 })
 
+app.delete('/api/notes/:id', (req, res)=> {
+    let noteToDelete = req.params.id;
+
+    fs.readFile("./db/db.json", (err, data) => {
+        if(err) throw err;
+        let arr = JSON.parse(data)
+
+        for(i=0; i<arr.length; i++){
+            if(noteToDelete == arr[i].id){
+                arr.splice(i, 1);
+                fs.writeFile("./db/db.json", JSON.stringify(arr), err => {
+                    if(err) throw err
+                })
+            }
+        }
+
+        res.json(arr)
+    })
+})
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 })
